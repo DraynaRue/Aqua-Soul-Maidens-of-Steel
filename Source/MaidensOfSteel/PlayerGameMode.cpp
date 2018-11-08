ac3f2a02d22b666e9dfbb59b0eb31cd3c397edad
DraynaRue <mattie0@hotmail.co.uk>
2018-11-08 17:50:18 +0000

    Created player 2

--- Source/MaidensOfSteel/PlayerGameMode.cpp
+++ Source/MaidensOfSteel/PlayerGameMode.cpp
@@ -1,10 +1,10 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PlayerGameMode.h"
-#include "PlayerPawn.h"
+#include "PlayerCharacter.h"
 
 APlayerGameMode::APlayerGameMode() 
 {
 	// set default pawn class to our character class
-	DefaultPawnClass = APlayerPawn::StaticClass();
+	DefaultPawnClass = APlayerCharacter::StaticClass();
 }
