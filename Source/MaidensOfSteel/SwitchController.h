d66e2da1fd36bda897c4c84712b224ee6dd5abb2
DraynaRue <mattie0@hotmail.co.uk>
2018-11-12 15:34:32 +0000

    Started on custom player controller

---
+++ Source/MaidensOfSteel/SwitchController.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "SwitchController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MAIDENSOFSTEEL_API ASwitchController : public APlayerController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
